5f4b19adc57c91a1ff211ea94a547a7f1d429fcf
Codú <134159765+BrayanBCode@users.noreply.github.com>
2026-08-06 18:25:58 -0300

    Iniciando github

---
+++ src/main/java/com/tecnologoinf/progapp/grupo8/s4/ProgAppGrupo8S42026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tecnologoinf.progapp.grupo8.s4;
+
+/**
+ *
+ * @author briha
+ */
+public class ProgAppGrupo8S42026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
