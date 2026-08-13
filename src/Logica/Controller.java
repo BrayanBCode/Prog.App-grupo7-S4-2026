14738069fca186fe1ca78102d1c5b7ef6031e308
Codú <134159765+BrayanBCode@users.noreply.github.com>
2026-08-13 18:49:56 -0300

    Nos mudamos a Ant

---
+++ src/Logica/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+/**
+ *
+ * @author maida
+ */
+public class Controller implements IController {
+
+    @Override
+    public void hola() {
+        System.out.print("Hola mundito de geys");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import Presentacion.JFInicio;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author briha
+ */
+public class Main {
+    public static void main() {
+        new JFInicio().setVisible(true);
+    }
+}
